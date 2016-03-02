4b18f0c38242cfb0d02e10563bc740d4f4cf693f
vagabond1-1983 <vagabond1_1983@163.com>
2016-03-02 22:52:30 +0800

    first demo

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # webUIPractice
 web UI实战
 
+- 不同浏览器访问baidu
+    - Firefox
+    - Chrome
+    - IE
+- API
+    - WebDriver
+    - WebElement
+
 
--- src/main/java/com/test/java/webui/accessBaiduDemo/firefoxAccessBaidu.java
+++ src/main/java/com/test/java/webui/accessBaiduDemo/firefoxAccessBaidu.java
@@ -1,4 +1,4 @@
-package com.test.java.webui.api;
+package com.test.java.webui.accessBaiduDemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by beigui on 2016/3/2.
  */
-public class firstDemo {
+public class firefoxAccessBaidu {
     public static void main(String[] args) {
         //Firefox如果不在默认的位置 %PROGRAMFILES%\Mozilla Firefox\firefox.exe
 //        System.setProperty("webdriver.firefox.bin","D:\\Program Files\\Mozilla Firefox\\firefox.exe");
