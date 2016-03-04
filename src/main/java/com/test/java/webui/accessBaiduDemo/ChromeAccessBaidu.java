283aa8d39ec7a90a0f58a05203369d52de61dd7c
vagabond1-1983 <vagabond1_1983@163.com>
2016-03-04 15:23:47 +0800

    +IE

---
+++ src/main/java/com/test/java/webui/accessBaiduDemo/ChromeAccessBaidu.java
@@ -0,0 +1,17 @@
+package com.test.java.webui.accessBaiduDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by beigui on 2016/3/4.
+ */
+public class ChromeAccessBaidu {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","d:\\Software\\SeleniumDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.baidu.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
---
+++ src/main/java/com/test/java/webui/accessBaiduDemo/IEAccessBaidu.java
@@ -0,0 +1,18 @@
+package com.test.java.webui.accessBaiduDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+/**
+ * Created by beigui on 2016/3/4.
+ * 功能：IE访问百度，需要Ie driver
+ */
+public class IEAccessBaidu {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.ie.driver", "d:\\Software\\SeleniumDriver\\IEDriverServer.exe");
+        WebDriver driver = new InternetExplorerDriver();
+        driver.get("http://www.baidu.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
