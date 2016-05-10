4ee54aa6758543e1d69e5d28ccf65cb7fb85cef0
vagabond1-1983 <vagabond1_1983@163.com>
2016-05-10 23:14:08 +0800

    update

--- src/main/java/com/test/java/unit/junit/AddFunctionServiceTest.java
+++ src/main/java/com/test/java/unit/junit/AddFunctionServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 
 /**
  * Created by beigui on 2016/5/10.
+ * 单元测试是一个测试思想，比方例子中的对AddFunctionService的add方法进行测试，就是一个单元测试，是最小单元的测试
+ * 而Junit或者说unitX，是框架，为了让单元测试更简单且复用性高而建立的
+ * 在这个例子中，现阶段无法体现出来。
+ * 让我们扩展AddFunctionService，添加更多的方法，展示框架的强大之处
  */
 public class AddFunctionServiceTest {
     private AddFunctionService addFunctionService;
