fb0ad4450f5ee21cdd2fcfa6aa2b46afc3fa6402
vagabond1-1983 <vagabond1_1983@163.com>
2016-06-01 07:00:18 +0800

    update

--- src/main/java/com/test/java/interfaceTest/SimpleServiceTest.java
+++ src/main/java/com/test/java/interfaceTest/SimpleServiceTest.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,6 +23,7 @@ import java.util.List;
  * Restful API Address: http://www.sosoapi.com/pass/apidoc/demo.htm
  * HttpClient example: http://www.mkyong.com/java/apache-httpclient-examples/
  * Apache ORG: https://hc.apache.org/httpcomponents-client-ga/quickstart.html
+ * IBM的一篇实例文章 http://www.ibm.com/developerworks/cn/aix/library/au-aix-systemsdirector/
  */
 public class SimpleServiceTest {
     @DataProvider(name = "urls")
@@ -37,6 +39,13 @@ public class SimpleServiceTest {
                 {"http://www.sosoapi.com/demo/user/simple/add.htm", "111", "e33"}
         };
     }
+
+    @DataProvider(name = "delDatas")
+    public Object[][] delDatas() {
+        return new Object[][]{
+                {"http://www.sosoapi.com/demo/user/simple/1/del.htm"}
+        };
+    }
   
     @Test(dataProvider = "urls")
     public void getTest(String url) throws IOException {
@@ -49,6 +58,26 @@ public class SimpleServiceTest {
         urlParams.add(new BasicNameValuePair("email", email));
         urlParams.add(new BasicNameValuePair("nickName", nickName));
         System.out.println(postJsonRes(url, new UrlEncodedFormEntity(urlParams)));
+    }
+
+    @Test(dataProvider = "delDatas")
+    public void delTest(String url) throws IOException {
+        System.out.println(delJsonRes(url));
+    }
+
+    private String delJsonRes(String url) throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpDelete delete = new HttpDelete(url);
+
+        CloseableHttpResponse response = client.execute(delete);
+        System.out.println("Response code: " + response.getStatusLine().getStatusCode());
+
+        try {
+            String resString = EntityUtils.toString(response.getEntity());
+            return JSON.toJSONString(resString);
+        } finally {
+            client.close();
+        }
     }
 
     private String postJsonRes(String url, HttpEntity entity) throws IOException {
